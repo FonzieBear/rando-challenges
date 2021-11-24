668ccf8ecb0549f2b40b98c9fcff93b947d5fbd3
Seju Bidiwala <sbbidiwala@gmail.com>
2021-11-24 14:34:57 -0600

    Add test cases

--- phone-number-spellings.py
+++ phone-number-spellings.py
@@ -35,4 +35,20 @@ def spell_number(phone):
 
     return perms
 
-print(spell_number('18002277387'))
+def test_spell_number(input, expected_length):
+    result = spell_number(input)
+    num_results = len(result)
+    if (num_results > 20):
+        result = '%s ... (truncated)' % result[:20]
+    print('input: %s\noutput length: %s\noutput: %s' % (input, result, num_results))
+    if num_results != expected_length:
+        print('FAILED. Expected output lenght of %s' % expected_length)
+    else:
+        print('PASSED')
+
+
+
+test_spell_number('2222', 121)          # 1 + 3^1 + 3^2 + 3^3 + 3^4 = 160
+test_spell_number('0000000000', 1)
+test_spell_number('99999999991', 1)
+test_spell_number('18002277387', 7685)     # 1 + 4^1 + 4*3 + 4*3^2 + 4^2*3^2 + 4^3*3^2 + 4^3*3^3 + 4^3*3^4 = 7685
